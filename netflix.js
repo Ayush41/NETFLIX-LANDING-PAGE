030e8206096b47ee8af9919b0e59b742b9ce2026
ayush41 <bhattayushraj@gmail.com>
2023-11-02 23:54:27 +0530

    FAQ section done

--- netflix.js
+++ netflix.js
@@ -1,9 +1,14 @@
 // alert("JS is connected");
 console.log("JS is connected ");
 const langDropdown = document.getElementById("language-btn");
-
+const faqlist = document.querySelectorAll(".inner-container ul li");
 
 langDropdown.addEventListener("click", ()=>{
     document.getElementById("language-dropdown").classList.toggle("show");
 });
 
+faqlist.forEach((item)=>{
+    item.addEventListener("click", (e) =>{
+        item.classList.toggle("show");
+    })
+})
