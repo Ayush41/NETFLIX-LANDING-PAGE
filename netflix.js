39f669a293829060c70f9ccef3b4e4bf29c35d4d
ayush41 <bhattayushraj@gmail.com>
2023-11-02 23:17:28 +0530

    footer completed

--- netflix.js
+++ netflix.js
@@ -1,1 +1,9 @@
-console.log("JS is connected");
+// alert("JS is connected");
+console.log("JS is connected ");
+const langDropdown = document.getElementById("language-btn");
+
+
+langDropdown.addEventListener("click", ()=>{
+    document.getElementById("language-dropdown").classList.toggle("show");
+});
+
